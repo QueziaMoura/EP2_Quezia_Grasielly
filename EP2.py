c571a40beb380c919fca68069bab3c6cdf270814
GrasiellyAlmeida <79549546+GrasiellyAlmeida@users.noreply.github.com>
2021-05-11 14:38:10 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -147,10 +147,10 @@ while True:
     baralho = empilha(baralho,i,i-3)
 
   if len(movimentos) == 2:
-    print(style.YELLOW + "Sobre qual carta você quer empilhar o {}".format(baralho[i]))
+    print(style.YELLOW + "Sobre qual carta você quer empilhar o {}".format(baralho[i]) + style.RESET)
 
-    print(style.GREEN + "1. {}".format(baralho[i-1]))
-    print(style.GREEN + "2. {}".format(baralho[i-3]))
+    print(style.GREEN + "1. {}".format(baralho[i-1]) + style.RESET)
+    print(style.GREEN + "2. {}".format(baralho[i-3]) + style.RESET)
 
     decisao = int(input("Digite um número de sua escolha (1 ou 2): "))
     if decisao == 1:
