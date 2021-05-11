1e887be4ca479aaf611606323c64226a1cacd42c
GrasiellyAlmeida <79549546+GrasiellyAlmeida@users.noreply.github.com>
2021-05-11 18:30:01 -0300

    Update EP2.py

--- EP2.py
+++ EP2.py
@@ -152,51 +152,11 @@ while True:
       baralho = empilha(baralho,i,i-3)
       
   if len(movimentos) == 2:
-    print(style.YELLOW + "Sobre qual carta você quer empilhar o {}?".format(baralho[i]) + style.RESET)
+    print("Sobre qual carta você quer empilhar o {}".format(baralho[i]))
 
-      if 1 < i < len(baralho):
-
-      if 1 <= i < len(baralho):
-
-        movimentos = lista_movimentos_possiveis(baralho,i)
-        if len(movimentos) != 0:
-          break
-        if len(movimentos) == 0:
-          i = int(input("A carta {} não pode ser movida. Por favor, digite um número entre 1 e {}: ".format(baralho[i],len(baralho))))
-          i = i - 1
-        
-      if not (1 <= i < len(baralho)):
-        i = int(input("Posição inválida. Por favor, digite um número entre 1 e {}: ".format(len(baralho))))
-        i = i - 1
-       
-#########################################################################
-  
-    if len(movimentos) == 1 :
-      if movimentos[0] == 1 : 
-        baralho = empilha(baralho,i,i-1)
-      baralho = empilha(baralho,i,i-3)
-
-    if len(movimentos) == 2:
-      print(style.YELLOW + "Sobre qual carta você quer empilhar o {}?".format(baralho[i]) + style.RESET)
-
-
-      print(style.GREEN + "1. {}".format(baralho[i-1]) + style.RESET)
-      print(style.GREEN + "2. {}".format(baralho[i-3]) + style.RESET)
-
-
-
-  while True:  
-    decisao = int(input("Digite um número de sua escolha (1 ou 2): "))
-    if decisao == 1:
-      baralho = empilha(baralho,i,i-1)
-    if decisao == 2:
-      baralho = empilha(baralho,i,i-3)
-
-      decisao = int(input("Digite um número de sua escolha (1 ou 2): "))
-      if decisao == 1:
-        baralho = empilha(baralho,i,i-1)
-      if decisao == 2:
-        baralho = empilha(baralho,i,i-3)
+    print("1. {}".format(baralho[i-1]))
+    print("2. {}".format(baralho[i-3]))  
+         
 
       while True:
           decisao = int(input("Digite um número de sua escolha (1 ou 2): "))
